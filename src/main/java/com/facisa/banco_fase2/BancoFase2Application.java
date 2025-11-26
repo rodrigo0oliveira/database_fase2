248b66820d5b059a05a5c6355d75331fe97ae533
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2025-11-26 17:53:14 -0300

    First commit

---
+++ src/main/java/com/facisa/banco_fase2/BancoFase2Application.java
@@ -0,0 +1,13 @@
+package com.facisa.banco_fase2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoFase2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoFase2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/facisa/banco_fase2/BancoFase2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facisa.banco_fase2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoFase2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
